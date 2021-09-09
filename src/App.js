0f272566e4615c028485f4f7997df9bed2dc821a
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-09-09 21:04:04 +1000

    First version of HN without the points

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [stories, setStories] = useState([]);
+  const [message, setMessage] = useState('loading...');
+  useEffect(() => {
+    async function fetchNewsStories () {
+      try {
+        const data = await (await fetch('https://hn.algolia.com/api/v1/search_by_date?tags=front_page')).json();
+        setStories(data.hits)
+        const message = data.hits.length ? '' : 'No stories found';
+        setMessage(message);
+      } catch (err) {
+        console.log(`err: ${err.mesasge}`, err);
+        setMessage('could not fetch stories');
+      }
+    }
+    fetchNewsStories()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Latest HN Stories</h2>
+        {message}
+        <div className="stories">
+          {Array.isArray(stories) && stories.map(
+            story => story.url && <h3><a href={story.url} target="_blank" rel="noreferrer">{story.title}</a> - by {story.author}</h3>
+          )}
+        </div>
       </header>
     </div>
   );
